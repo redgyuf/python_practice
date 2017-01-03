d0e27a6ea7ebbc6b337d155662ef022ff2cf0866
redgyuf <faramirex070@gmail.com>
2017-01-03 09:29:34 +0100

    small edit

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,9 +2,6 @@ number_list = [3, 7, 4, 9, 5, 2, 6, 1]
 
 
 def insertion_sort(num_list):
-    sorted_list = list()
-    sorted_list.append(num_list[0])
-
     for i in range(1, len(num_list)):
         x = num_list[i]
         j = i - 1
